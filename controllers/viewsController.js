31e7124b6e29bc4633aed70a3c067512535833e4
David <david@Davids-MacBook-Pro.local>
2022-05-05 09:56:41 +0900

    Implement bar graph for sorting algorithm. Implement bubble sort, insertion sort, selection sort and heap sort visualization algorithm.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 exports.getOverviewPage = function(req, res) {
-    res.sendFile(path.resolve("public/overview.html"));
+    res.status(200).sendFile(path.resolve("public/overview.html"));
 };
 
 exports.getPathfindingPage = function(req, res) {
-    res.sendFile(path.resolve("public/pathfindingAlgorithm.html"));
+    res.status(200).sendFile(path.resolve("public/pathfindingAlgorithm.html"));
 };
 
 exports.getSortingPage = function(req, res) {
-    res.sendFile(path.resolve("public/sortingAlgorithm.html"));
+    res.status(200).sendFile(path.resolve("public/sortingAlgorithm.html"));
 };
